4f6efe07065ef078aa1e34a8bf107ab29f50a2d8
Medido1 <medi1222@gmail.com>
2025-06-21 20:25:54 +0100

    Add animation to home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,27 +1,36 @@
+import { motion, AnimatePresence } from "framer-motion";
+
 function HomePage() {
   return (
-    <div>
-      <div className="mt-10 sm:mt-20 flex flex-col items-center
-        lg:mt-[7%] 2xl:mt-[14%] lg:flex-row lg:justify-between lg:px-[10%] lg:pb-[5%]">
-        <div>
-          <h1 className="text-center text-xl xl:text-2xl text-[hsl(var(--clr-blue))] uppercase tracking-widest
-          font-condensed mb-4 lg:text-left">
-            So, you want to travel to
-          <span className="text-8xl xl:text-9xl block text-white mt-8 font-bellefair">Space</span></h1>
-          <p className="text-center lg:text-left text-lg/8 xl:text-xl text-[hsl(var(--clr-blue))] 
-            font-condensed sm:w-[55ch]">
-            Let’s face it; if you want to go to space, you might as well genuinely go to
-            outer space and not hover kind of on the edge of it. Well sit back, and relax
-            because we’ll give you a truly out of this world experience!
-          </p>
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.9 }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="mt-10 sm:mt-20 flex flex-col items-center
+          lg:mt-[7%] 2xl:mt-[14%] lg:flex-row lg:justify-between lg:px-[10%] lg:pb-[5%]">
+          <div>
+            <h1 className="text-center text-xl xl:text-2xl text-[hsl(var(--clr-blue))] uppercase tracking-widest
+            font-condensed mb-4 lg:text-left">
+              So, you want to travel to
+            <span className="text-8xl xl:text-9xl block text-white mt-8 font-bellefair">Space</span></h1>
+            <p className="text-center lg:text-left text-lg/8 xl:text-xl text-[hsl(var(--clr-blue))]
+              font-condensed sm:w-[55ch]">
+              Let’s face it; if you want to go to space, you might as well genuinely go to
+              outer space and not hover kind of on the edge of it. Well sit back, and relax
+              because we’ll give you a truly out of this world experience!
+            </p>
+          </div>
+          <button className="flex justify-center items-center px-10 py-16
+            rounded-[50%] aspect-square text-black bg-white mt-40 sm:mt-20 text-lg sm:text-3xl
+            xl:text-4xl">
+            <a className="uppercase tracking-widest" href="">Explore</a>
+          </button>
         </div>
-        <button className="flex justify-center items-center px-10 py-16
-          rounded-[50%] aspect-square text-black bg-white mt-40 sm:mt-20 text-lg sm:text-3xl
-          xl:text-4xl">
-          <a className="uppercase tracking-widest" href="">Explore</a>
-        </button>
-      </div>
-    </div>
+      </motion.div>
+    </AnimatePresence>
   )
 }
 
