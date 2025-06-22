f56cd3aff56ffac43a4e1ba975ed2aa6b865a3c6
Medido1 <medi1222@gmail.com>
2025-06-22 16:49:13 +0100

    Fix style issues

--- src/components/Crew.jsx
+++ src/components/Crew.jsx
@@ -9,47 +9,52 @@ function Crew() {
   return (
     <div className="flex flex-col items-center mt-[20%] lg:mt-[4%]">
       <h2 className="sm:self-start font-condensed  text-2xl uppercase text-gray-200 tracking-widest
-        lg:ml-[10%] xl:mb-[2%]">
+        lg:ml-[10%] xl:mb-0">
         <span className="mr-6 text-gray-600">02</span>
         Meet your crew
       </h2>
       <AnimatePresence mode="wait">
         <motion.div
-          className="flex flex-col items-center mt-[20%]"
+          className="mt-[20%] lg:flex  lg:mt-0 lg:w-[80%]"
           key={currentCrewMember.name}
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: -20 }}
-          transition={{ duration: 0.5, ease: "easeInOut" }}
+          transition={{ duration: 0.5, ease: "easeInOut", delay: .2 }}
         >
-          <h2
-            className="text-2xl text-[hsl(var(--clr-blue)_/_0.4)] uppercase font-condensed"
-          >
-            {currentCrewMember.role}
-          </h2>
-          <h1
-            className="text-center text-2xl lg:text-4xl font-bellefair 
-              uppercase mb-[10%] mt-[2%]"
-          >
-            {currentCrewMember.name}
-          </h1>
-          <p
-            className="max-w-[50ch] text-center text-xl font-condensed text-[hsl(var(--clr-blue))]
-                lg:text-left lg:text-lg"
-          >
-            {currentCrewMember.bio}
-          </p>
-          <ul className="flex gap-4 mt-[10%]">
-            {crew.map((member, index) => 
-            <li
-              key={member.name}
-              onClick={() => setCurrentCrewMember(crew[index])}
-              className= {`p-2 aspect-square rounded-[50%] mb-[50%]
-              ${currentCrewMember.name === member.name ? "bg-white" : "bg-gray-400"} `}
+          <div className="flex flex-col items-center lg:items-start lg:mt-[10%]">
+            <h2
+              className="text-2xl lg:text-3xl text-[hsl(var(--clr-blue)_/_0.4)] uppercase font-condensed"
             >
-            </li>)}
-          </ul>
-          <img src={currentCrewMember.images.webp} alt={currentCrewMember.name} />
+              {currentCrewMember.role}
+            </h2>
+            <h1
+              className="text-center lg:text-left text-2xl lg:text-4xl xl:text-5xl font-bellefair
+                uppercase mb-[10%] mt-[2%] lg:mb-[5%]"
+            >
+              {currentCrewMember.name}
+            </h1>
+            <p
+              className="max-w-[50ch] text-center text-xl font-condensed text-[hsl(var(--clr-blue))]
+                  lg:text-left lg:text-lg xl:text-xl sm:mb-[10%] lg:mb-[20%]"
+            >
+              {currentCrewMember.bio}
+            </p>
+            <ul className="flex gap-4 lg:gap-8">
+              {crew.map((member, index) =>
+              <li
+                key={member.name}
+                onClick={() => setCurrentCrewMember(crew[index])}
+                className= {`p-2 aspect-square rounded-[50%] mb-[50%] cursor-pointer hover:bg-white
+                ${currentCrewMember.name === member.name ? "bg-white" : "bg-gray-400"} `}
+              >
+              </li>)}
+            </ul>
+          </div>
+          <img 
+            className="sm:w-[80%] mx-auto lg:mr-auto lg:w-[30%] lg:max-h-[500px]"
+            src={currentCrewMember.images.webp} alt={currentCrewMember.name} 
+          />
         </motion.div>
       </AnimatePresence>
     </div>
